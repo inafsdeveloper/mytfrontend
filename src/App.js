fa7b2f604b6dd531a0d2f8496cb406c5f1aff498
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-19 23:21:33 -0700

    Added Basic Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
+
+import SubscriptionsList from './subscription/pages/SubscriptionsList';
+import VideosList from './videos/pages/VideosList';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Router>
+        <Switch>
+          <Route path="/subscriptions" exact>
+            <SubscriptionsList />
+          </Route>
+          <Route path="/s1/videos" exact>
+            <VideosList />
+          </Route>
+          <Redirect to="/subscriptions" />
+        </Switch>
+      </Router>
+    </main>
   );
 }
 
---
+++ src/videos/pages/VideosList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const VideosList = props => {
+    return (
+        <h2> Videos List </h2>
+    );
+};
+
+export default VideosList;
