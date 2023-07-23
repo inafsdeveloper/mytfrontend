a20e306ca354a3915bc156ace53fc53eb7cfce53
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-23 00:57:55 -0700

    Modified videso layout and added search video capability

--- src/videos/components/Video.js
+++ src/videos/components/Video.js
@@ -7,17 +7,16 @@ const Video = props => {
     const channelThumbnail = props.channelThumbnail;
     const YOUTUBE_BASE_VIDEO_URL = "https://www.youtube.com/embed";
     return (
-        <l1 className="list-group-item" style={{width: '80%'}}>
-            <div class="ratio ratio-16x9">
+        <div class="row embed-responsive embed-responsive-16by9" style={{padding: "10px"}}>
                 <iframe
+                    className="embed-responsive-item"
                     src={`${YOUTUBE_BASE_VIDEO_URL}/${props.item.id.videoId}`}
                     title="YouTube video" allowfullscreen>
                 </iframe>
-            </div>
-            <div>
-                <h5>{snippet.title}</h5>
-            </div>
-        </l1>
+        </div>
+
+
+
 
     );
 
--- src/videos/pages/VideosList.js
+++ src/videos/pages/VideosList.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Video from '../components/Video';
+import Search from "../../shared/components/UIElements/Search";
 
-const videsData = require('../../shared/data/videos-data.json');
+const DummyVidesData = require('../../shared/data/videos-data.json');
 
 const VideosList = props => {
+    const [searchText, setSearchText] = useState();
+    const [videosData, setVidoesData] = useState(DummyVidesData.items);
+
+    const inputChangeHandler = event => {
+        setSearchText(event.target.value);
+    };
+
+    const searchHandler = event => {
+        event.preventDefault();
+        const filteredData = DummyVidesData.items.filter(item => item.snippet.title.includes(searchText));
+        setVidoesData(filteredData);
+    };
+
     return (
-        <div className="container">
-        <div class="row">
-            <ul className="list-group">
-                {videsData.items.map(item => (
+        <div id="videos" className="container">
+            <Search
+                inputValue={searchText}
+                handleInputChange={inputChangeHandler}
+                handleSearch={searchHandler}
+            />
+            <div class="row row-cols-auto justify-content-center">
+                {videosData.map(item => (
                     <Video
                         item={item}
                     />
                 ))}
-            </ul>
-
+            </div>
         </div>
-    </div>
     );
 };
 
