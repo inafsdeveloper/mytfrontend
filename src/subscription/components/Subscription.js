6ed7f8b3672c015792a20571b4533b6b075e87cc
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-22 22:37:14 -0700

    Modified Subscription presentation

--- src/subscription/components/Subscription.js
+++ src/subscription/components/Subscription.js
@@ -9,21 +9,24 @@ const Subscription = props => {
     const contentDeatils = props.item.contentDetails;
     console.log(snippet.title);
     return (
-        <l1 className="list-group-item">
-            <div className="card" style={{ width: '100%'}}>
-            <img
-                src={thumbnails.medium.url}
-                className="rounded-circle card-imp-top"
-                alt="..."
-            />
-            <div className="card-body">
-                <h5 className="card-title">{snippet.title}</h5>
-                {/* <h6 class="card-subtitle mb-2 text-muted">{snippet.description}</h6> */}
-                <p className="card-text">Total Videos: {contentDeatils.totalItemCount}</p>
+        // <l1 className="list-group-item">
+        <div className="col" style={{paddingBottom: '20px'}}>
+            <div className="card">
+                <img
+                    src={thumbnails.medium.url}
+                    className="rounded-circle card-imp-top"
+                    alt="..."
+                />
+                <div className="card-body">
+                    <h5 className="card-title">{snippet.title}</h5>
+                    {/* <h6 class="card-subtitle mb-2 text-muted">{snippet.description}</h6> */}
+                    <p className="card-text">Total Videos: {contentDeatils.totalItemCount}</p>
+                </div>
             </div>
         </div>
-        </l1>
-        
+
+        // </l1>
+
     );
 };
 
--- src/subscription/pages/SubscriptionsList.js
+++ src/subscription/pages/SubscriptionsList.js
@@ -6,29 +6,29 @@ import Search from "../../shared/components/UIElements/Search";
 const subscriptionDATA = require('../../shared/data/subs-data.json');
 const SubscriptionsList = props => {
 
-    return (
-        <div className="card-group">
-            {subscriptionDATA.items.map(item => (
-                        <Subscription
-                            item={item}
-                        />
-                    ))}
-        </div>
-    );
     // return (
-    //     <div className="container">
-    //         <div class="row">
-    //             <ul className="list-group">
-    //                 {subscriptionDATA.items.map(item => (
+    //     <div className="card-group">
+    //         {subscriptionDATA.items.map(item => (
     //                     <Subscription
     //                         item={item}
     //                     />
     //                 ))}
-    //             </ul>
-
-    //         </div>
     //     </div>
     // );
+    return (
+        <div className="container">
+            <div class="row row-cols-auto">
+                {/* <ul className="list-group"> */}
+                    {subscriptionDATA.items.map(item => (
+                        <Subscription
+                            item={item}
+                        />
+                    ))}
+                {/* </ul> */}
+
+            </div>
+        </div>
+    );
 };
 
 export default SubscriptionsList;
